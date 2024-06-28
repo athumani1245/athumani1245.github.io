cf09c5eb3fb3ef197c709e588c782a6db3d4b88a
athumani1245 <athumanishabani18@gmail.com>
2024-06-28 15:09:47 +0300

    first changes

---
+++ js/events-slider.js
@@ -0,0 +1,590 @@
+$( window ).on( "load", function() {
+     $('.march-event-2022').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+    
+    $('.may-event-2022').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+    
+    $('.june-event-2022').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+    
+    $('.octuber-event-2022').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+    
+    $('.june-event-2023').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+    
+	$('#event-july-2021').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+    
+	$('#event-june-2021').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+
+    $('#event-july-2019').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+
+    $('#event-oct-2019').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+
+    $('#event-sept-2019').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+
+    $('#event-june-2019').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+
+    $('#event-jan-2019').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+
+    $('#event-jan-2019-1').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+
+    $('#event-dec-2019').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+
+    $('#event-oct-2018').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+
+    $('#event-jan-2018').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+
+    $('#event-april-2016').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+
+    $('#event-april-2016-1').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+    
+    $('.march-event-2023').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+    
+    $('.november-event-2022').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+    
+    $('.october-event-2022').owlCarousel({
+      	items:4,
+      	loop:true,
+      	// center:true,
+      	autoWidth:true,
+      	nav: true,
+      	navText: ["<img src='img/arrow-prev.png'>","<img src='img/arrow-next.png'>"],
+      	margin:10,
+      	autoplayHoverPause:false,
+      	autoplay:true,
+      	autoplaySpeed: 1000,
+
+	     responsive:{
+	        0:{
+	          nav: false,
+	          items:1
+	        },
+	        600:{
+	          nav: false,
+	          items:2
+	        },
+	        1000:{
+	          nav: true,
+	          items:3
+	        }
+	     }
+    });
+
+});
